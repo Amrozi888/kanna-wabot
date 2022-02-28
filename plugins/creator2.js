d6b7c88e543e8dd254e887805f8dabc55358744a
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-02-28 17:20:04 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -3,7 +3,7 @@ const PhoneNumber = require('awesome-phonenumber')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, {
     contacts: [{
-    "displayName": "ᴏᴡɴᴇʀ ᴋᴀɴɴᴀʙᴏᴛ",
+    "displayName": "owner-botynyakamu",
       "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ᴏᴡɴᴇʀ ᴋᴀɴɴᴀʙᴏᴛ\nitem1.TEL;waid=62831433937633:62831433937633\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:zeeoneofc@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://hardianto.xyz/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ 𝗞𝗔𝗡𝗡𝗔𝗕𝗢𝗧 ]───────\nEND:VCARD"
   }, {
     "displayName": "Whatsapp",
